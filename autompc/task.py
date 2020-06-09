6472f6a43b9997de029f2ba7dc5dc47e3f0adec8
William Edwards <williamedwards314@gmail.com>
2020-06-08 19:26:24 -0700

    Working commit on tasks.

--- autompc/task.py
+++ autompc/task.py
@@ -9,18 +9,61 @@ class Task:
         self._qp = False
         self._conv = False
 
+        # Cost types:
+        # 0 : None
+        # 1 : Quadratic
+        # 2 : Convex
+        # 3 : Diff
+        # 4 : Numeric
+
+        self.cost_type = 0
+
     # Adding Costs
-    def set_quad_cost(self):
-        pass
+    def set_quad_cost(self, Q, R, F=None):
+        if Q.shape != (self.system.obs_dim, self.system.obs_dim):
+            raise ValueError("Q is the wrong shape")
+        if R.shape != (self.system.ctrl_dim, self.system.ctrl_dim):
+            raise ValueError("R is the wrong shape")
+        if not F is None:
+            if F.shape != (self.system.obs_dim, self.system.obs_dim):
+                raise ValueError("F is the wrong shape")
+        else:
+            F = np.zeros((self.system.obs_dim, self.system.obs_dim))
+        self.cost_type = 1
+        self._quad_cost = Q, R, F
 
-    def set_convex_cost(self):
-        pass
+    def set_convex_cost(self, add_state_cost, add_ctrl_cost, terminal_state_cost):
+        """
+        all arguments are functions with the
+        signature
+            state/ctrl -> cost, grad
+        """
+        self.cost_type = 2
+        self._add_state_cost = add_state_cost
+        self._add_ctrl_cost = add_ctrl_cost
+        self._terminal_state_cost = terminal_state_cost
 
-    def set_diff_cost(self):
-        pass
+    def set_diff_cost(self, add_state_cost, add_ctrl_cost, terminal_state_cost):
+        """
+        all arguments are functions with the
+        signature
+            state/ctrl -> cost, grad
+        """
+        self.cost_type = 3
+        self._add_state_cost = add_state_cost
+        self._add_ctrl_cost = add_ctrl_cost
+        self._terminal_state_cost = terminal_state_cost
 
-    def set_numeric_cost(self):
-        pass
+    def set_numeric_cost(self, add_state_cost, add_ctrl_cost, terminal_state_cost):
+        """
+        all arguments are functions with the
+        signature
+            state/ctrl -> cost
+        """
+        self.cost_type = 4
+        self._add_state_cost = add_state_cost
+        self._add_ctrl_cost = add_ctrl_cost
+        self._terminal_state_cost = terminal_state_cost
 
     # Adding Constraints
     def add_state_bound(self, state, lower, upper):
