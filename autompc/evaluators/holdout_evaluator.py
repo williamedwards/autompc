00301f696c80e00c8c203c8fd519696978a06250
William Edwards <wre2@illinois.edu>
2020-07-26 00:57:09 -0700

    Added graphs

--- autompc/evaluators/holdout_evaluator.py
+++ autompc/evaluators/holdout_evaluator.py
@@ -31,11 +31,14 @@ class HoldoutEvaluator(Evaluator):
         primary_metric_values = np.zeros(len(self.holdout))
         secondray_metric_values = np.zeros((len(self.holdout),
             len(self.secondary_metrics)))
+        graphs = [grapher(model, configuration) for grapher in self.graphers]
         for i, traj in enumerate(self.holdout):
             predictor = CachingPredictor(traj, m)
             primary_metric_values[i] = self.primary_metric(predictor, traj)
             for j, metric in enumerate(self.secondary_metrics):
                 secondary_metric_values[i, j] = metric(predictor, traj)
+            for graph in graphs:
+                graph.add_traj(predictor, traj)
 
         primary_metric_value = self.primary_metric.accumulate(primary_metric_values)
         secondary_metric_value = np.array(len(self.secondary_metrics))
@@ -49,4 +52,4 @@ class HoldoutEvaluator(Evaluator):
         print(configuration)
         print("score = {}".format(primary_metric_value))
 
-        return primary_metric_value, secondary_metric_value
+        return primary_metric_value, secondary_metric_value, graphs
