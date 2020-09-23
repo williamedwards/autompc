fix jacobian issue with scaling

autompc/sysid/largegp.py
@@ -47,12 +47,14 @@ class BatchIndependentMultitaskGPModel(gpytorch.models.ExactGP):
 
 
 class LargeGaussianProcess(Model):
-    def __init__(self, system, mean='constant', kernel='RBF', niter=100, lr=0.1):
+    def __init__(self, system, mean='constant', kernel='RBF', niter=40, lr=0.1):
         super().__init__(system)
         self.gpmodel = BatchIndependentMultitaskGPModel(self.system.obs_dim, mean, kernel).double()
         self.niter = niter
         self.lr = lr
         self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+        if torch.cuda.is_available():
+            print("Cuda is used for LargeGaussianProcess")
         self.gpmodel = self.gpmodel.to(self.device)
 
     @staticmethod
@@ -138,7 +140,7 @@ class LargeGaussianProcess(Model):
         predy.backward(torch.eye(obs_dim).to(self.device))
         jac = TsrXt.grad.cpu().data.numpy()
         # properly scale back...
-        jac = jac * self.xu_std[None]  # a row one for broadcasting
+        jac = jac / self.xu_std[None]  # a row one for broadcasting
         # since repeat, y value is the first one...
         y = predy[0].cpu().data.numpy()
         n = self.system.obs_dim

examples/gp_grad_test.py
@@ -141,8 +141,8 @@ state = np.zeros(4,)
 state[0] = 0.5
 ctrl = np.ones(1,)
 x, state_jac, ctrl_jac = gp.pred_diff(state, ctrl)
-state_jac2 = fd_jac(lambda y: gp.pred(y, ctrl), state)
-ctrl_jac2 = fd_jac(lambda y: gp.pred(state, y), ctrl)
+state_jac2 = fd_jac(lambda y: gp.pred(y, ctrl), state, dt=1e-6)
+ctrl_jac2 = fd_jac(lambda y: gp.pred(state, y), ctrl, dt=1e-6)
 
 print("state_jac=")
 print(state_jac)

examples/gp_ilqr_test.py
@@ -1,3 +1,4 @@
+import time
 from pdb import set_trace
 import sys, os
 sys.path.append(os.getcwd() + "/..")
@@ -126,14 +127,31 @@ from autompc.control import IterativeLQR
 
 #@memory.cache
 def run_experiment():
-    #cs = LargeGaussianProcess.get_configuration_space(cartpole)
-    #cfg = cs.get_default_configuration()
-    #model = ampc.make_model(cartpole, LargeGaussianProcess, cfg)
-    #model.train(trajs[-2:])
-    cs = GaussianProcess.get_configuration_space(cartpole)
+    cs = LargeGaussianProcess.get_configuration_space(cartpole)
     cfg = cs.get_default_configuration()
-    model = ampc.make_model(cartpole, GaussianProcess, cfg)
-    model.train([trajs2[0][:100], trajs2[3][150:200]])
+    model = ampc.make_model(cartpole, LargeGaussianProcess, cfg)
+    model.train(trajs2[:3])
+
+    test_n = 100
+    x = np.array([0.1, 0, 0, 0])
+    u = np.array([0.])
+    t0 = time.time()
+    for i in range(test_n):
+        xn = model.pred(x, u)
+    print(time.time() - t0)
+
+    t0 = time.time()
+    for i in range(test_n):
+        xn, _, _ = model.pred_diff(x, u)
+    print(time.time() - t0)
+
+    raise
+
+
+    # cs = GaussianProcess.get_configuration_space(cartpole)
+    # cfg = cs.get_default_configuration()
+    # model = ampc.make_model(cartpole, GaussianProcess, cfg)
+    # model.train([trajs2[0][:100], trajs2[3][150:200]])
 
     # Now it's time to apply the controller
     task1 = ampc.Task(cartpole)
@@ -145,12 +163,13 @@ def run_experiment():
     hori = 40  # hori means integer horizon... how many steps...
     ubound = np.array([[-15], [15]])
     mode = 'auglag'
-    ilqr = IterativeLQR(cartpole, task1, model, hori, reuse_feedback=None)
+    ilqr = IterativeLQR(cartpole, task1, model, hori, reuse_feedback=None, verbose=True)
     # just give a random initial state
     sim_traj = ampc.zeros(cartpole, 1)
     x = np.array([0.1, 0, 0, 0])
     sim_traj[0].obs[:] = x
     us = []
+
 
     constate = ilqr.traj_to_state(sim_traj[:1])
     for step in range(200):

examples/ilqr_test.py
@@ -199,7 +199,7 @@ def test_sindy_cartpole():
         hori = 40  # hori means integer horizon... how many steps...
         ubound = np.array([[-15], [15]])
         mode = 'auglag'
-        ilqr = IterativeLQR(cartpole, task1, model, hori, reuse_feedback=None)
+        ilqr = IterativeLQR(cartpole, task1, model, hori, reuse_feedback=None, verbose=True)
         # just give a random initial state
         sim_traj = ampc.zeros(cartpole, 1)
         x = np.array([np.pi, 0, 0, 0])