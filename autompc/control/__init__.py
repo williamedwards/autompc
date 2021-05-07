95141565df0bafc06d02650cf16d5c5b50d080e3
William Edwards <wre2@illinois.edu>
2021-05-06 19:28:10 -0700

    Updated dependency handling of DT

--- autompc/control/__init__.py
+++ autompc/control/__init__.py
@@ -2,11 +2,10 @@
 #from .mpc import LinearMPC
 from .lqr import LQRFactory, FiniteHorizonLQR, InfiniteHorizonLQR
 from .ilqr import IterativeLQR, IterativeLQRFactory
-from .nmpc import DirectTranscriptionController, DirectTranscriptionControllerFactory
-#try:
-#    from .nmpc import NonLinearMPC
-#except ImportError:
-#    print("Missing dependency for NonLinearMPC")
+try: 
+    from .nmpc import DirectTranscriptionController, DirectTranscriptionControllerFactory
+except:
+    print("Missing dependency for NMPC")
 from .mppi import MPPI, MPPIFactory
 #from .mppi_adaptive import MPPIAdaptive
 #from .cem import CEM
