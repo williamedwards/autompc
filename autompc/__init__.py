6584f4571ef8abd94df6e5c883078d3ce0bac566
William Edwards <wre2@illinois.edu>
2021-05-29 12:43:15 -0700

    Debug changes

--- autompc/__init__.py
+++ autompc/__init__.py
@@ -1,3 +1,5 @@
+print("Loading AutoMPC...")
+
 from .sysid.model import Model
 from .system import System
 from .control.controller import Controller
@@ -5,3 +7,5 @@ from .trajectory import Trajectory, zeros, empty, extend
 from .tasks import Task
 from .utils import make_model, make_controller, simulate
 from .pipeline import Pipeline
+
+print("Finished loading AutoMPC")
