fa1979bdcc78903b7000cfc0de78afae46d7dfbb
William Edwards <wre2@illinois.edu>
2021-05-30 00:59:23 -0700

    Added setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
         name="autompc",
-        version="0.0.0",
+        version="0.0.1",
+        description="A library for the creation and tuning of System ID+MPC pipelines",
+        author="William Edwards",
+        author_email="williamedwards314@gmail.com",
+        url="https://github.com/williamedwards/autompc",
         packages=find_packages(include=["autompc", "autompc.*"])
 )
