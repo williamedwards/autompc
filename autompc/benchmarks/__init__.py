1ee537d748e56577b4924cf7a1f0fa1a6d23c117
William Edwards <wre2@illinois.edu>
2021-05-30 19:38:34 -0700

    Update import guard

--- autompc/benchmarks/__init__.py
+++ autompc/benchmarks/__init__.py
@@ -2,5 +2,5 @@ from .cartpole import CartpoleSwingupBenchmark
 from .cartpole_v2 import CartpoleSwingupV2Benchmark
 try:
     from .halfcheetah import HalfcheetahBenchmark
-except ImportError, ModuleNotFoundError:
+except (ImportError, ModuleNotFoundError) as e:
     pass
