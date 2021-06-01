ee63fdc499534a261419d45b43e726fabf243ae8
William Edwards <wre2@illinois.edu>
2021-05-31 22:54:42 -0700

    Remove old control code

--- autompc/control/__init__.py
+++ autompc/control/__init__.py
@@ -1,5 +1,3 @@
-#from .example import ExampleController
-#from .mpc import LinearMPC
 from .lqr import LQRFactory, FiniteHorizonLQR, InfiniteHorizonLQR
 from .ilqr import IterativeLQR, IterativeLQRFactory
 try:
@@ -8,5 +6,3 @@ except ImportError:
     print("Missing optional dependency for NMPC")
 from .mppi import MPPI, MPPIFactory
 from .zero import ZeroController
-#from .mppi_adaptive import MPPIAdaptive
-#from .cem import CEM
