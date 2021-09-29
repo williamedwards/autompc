48d0e993df5d811a68fc4eb17c2a0467a23abd50
ttchalakov <teodor.tch@gmail.com>
2021-09-29 10:30:14 -0500

    Added MLPDAD to init.py

--- autompc/sysid/__init__.py
+++ autompc/sysid/__init__.py
@@ -5,3 +5,4 @@ from .sindy import SINDy, SINDyFactory
 from .mlp import MLP, MLPFactory
 from .largegp import ApproximateGPModel, ApproximateGPModelFactory
 #from .linearize import LinearizedModel
+from .mlpdad import MLPDAD, MLPDADFactory
