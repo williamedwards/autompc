851211b824e420811a3144f89b3ae52526f101f5
ttchalakov <teodor.tch@gmail.com>
2022-04-19 00:00:52 -0500

    Vectorized barrier cost

--- autompc/costs/barrier_cost.py
+++ autompc/costs/barrier_cost.py
@@ -6,7 +6,7 @@ import numpy.linalg as la
 from .cost import Cost
 
 class LogBarrierCost(Cost):
-    def __init__(self, system, observation, limit, scale=1, upper=True):#TODO: Add controls cost
+    def __init__(self, system, boundedState):#TODO: Add controls cost
         """
         Create barrier cost that approximates an inequality constraint.
         where : - b * ln ( a - x ) for upper limit
@@ -17,29 +17,35 @@ class LogBarrierCost(Cost):
         system : System
             Robot system object.
 
-        observation : String
-            Observation name for which limit is specified.
+        boundedState : dict
+            Dictionary of { "observation or control name" : (limit, scale, upper)}
 
-        limit : Numpy array
-            limit value a that barrier is placed at.
+            observation : String
+                Observation name for which limit is specified.
 
-        scale : double
-            Positive scaler to magnify the cost function.
-            scale: (0, inf)
+            limit : double
+                limit value a that barrier is placed at.
 
-        upper : boolean
-            True if the limit is an upper limit.
-            False if the limit is a lower limit
+            scale : double
+                Positive scaler to magnify the cost function.
+                scale: (0, inf)
+
+            upper : boolean
+                True if the limit is an upper limit.
+                False if the limit is a lower limit
         """
         super().__init__(system)
-        self._obs_id = system.observations.index(observation)
-        self._limit = np.copy(limit)
-        self._direction = -1
-        self._scale = scale
-        if(upper):
-            self.direction = 1
-        
+        self.obsConfiguration = []
+        self.ctrlConfiguration = []
 
+        for variable in boundedState.keys():
+            if(variable in system.observations):
+                obs_id = system.observations.index(variable)
+                self.obsConfiguration.append([obs_id, boundedState[variable]])
+            if(variable in system.controls):
+                obs_id = system.controls.index(variable)
+                self.obsConfiguration.append([obs_id, boundedState[variable]])
+        
         # Configs
         self._is_quad = False
         self._is_convex = False     #TODO: Not sure
@@ -51,28 +57,64 @@ class LogBarrierCost(Cost):
     # - b * ln ( a - x ) upper limit
     # - b * ln ( a + x ) lower limit
     def eval_obs_cost(self, obs):
-        return -self._scale * np.log( self._limit - (self._direction * obs[self._obs_id]))
+        sum = 0
+        for boundedObs in self.obsConfiguration:
+            limit, scale, upper = boundedObs[1]
+            self._direction = -1
+            if(upper):
+                direction = 1
+            sum = sum + -scale * np.log(limit - (direction * obs[boundedObs[0]]))
+        return sum
 
     #Jacobian:
     # b / (a - x) upper limit
     # -b / (a - x) lower limit
     def eval_obs_cost_diff(self, obs):
-        return self.direction * self._scale / (self._limit - obs[self._obs_id])
+        jacobian = np.zeros(self.system.obs_dim)
+        for boundedObs in self.obsConfiguration:
+            limit, scale, upper = boundedObs[1]
+            self._direction = -1
+            if(upper):
+                direction = 1
+            jacobian[boundedObs[0]] = direction * scale / (limit - obs[boundedObs[0]])
+        return jacobian
 
     #Hessian:
     # b / (a - x)^2 upper limit
     # b / (a - x)^2 lower limit
     def eval_obs_cost_hess(self, obs):
-        return self._scale / ((self._limit - obs[self._obs_id])**2)
+        hessian = np.zeros((self.system.obs_dim, self.system.obs_dim))
+        for boundedObs in self.obsConfiguration:
+            limit, scale, upper = boundedObs[1]
+            hessian[boundedObs[0]][boundedObs[0]] = scale / ((limit - obs[boundedObs[0]])**2)
+        return hessian
 
     def eval_ctrl_cost(self, ctrl):
-        return 0
+        sum = 0
+        for boundedCtrl in self.ctrlConfiguration:
+            limit, scale, upper = boundedCtrl[1]
+            self._direction = -1
+            if(upper):
+                direction = 1
+            sum = sum + -scale * np.log(limit - (direction * ctrl[boundedCtrl[0]]))
+        return sum
     
     def eval_ctrl_cost_diff(self, ctrl):
-        return 0
+        jacobian = np.zeros(self.system.ctrl_dim)
+        for boundedCtrl in self.obsConfiguration:
+            limit, scale, upper = boundedCtrl[1]
+            self._direction = -1
+            if(upper):
+                direction = 1
+            jacobian[boundedCtrl[0]] = direction * scale / (limit - ctrl[boundedCtrl[0]])
+        return jacobian
 
     def eval_ctrl_cost_hess(self, ctrl):
-        return 0
+        hessian = np.zeros((self.system.obs_dim, self.system.obs_dim))
+        for boundedCtrl in self.obsConfiguration:
+            limit, scale, upper = boundedCtrl[1]
+            hessian[boundedCtrl[0]][boundedCtrl[0]] = scale / ((limit - ctrl[boundedCtrl[0]])**2)
+        return hessian
 
     def eval_term_obs_cost(self, obs):
         return 0
