013061e93bdb172bca7b2143ce833d389cfbac03
William Edwards <wre2@illinois.edu>
2022-04-25 22:08:46 -0500

    Updated costs.__init__

--- autompc/costs/__init__.py
+++ autompc/costs/__init__.py
@@ -1,8 +1,4 @@
 from .quad_cost import QuadCost
-from .quad_cost_factory import QuadCostFactory
-from .gauss_reg_factory import GaussRegFactory
 from .thresh_cost import ThresholdCost, BoxThresholdCost
 from .sum_cost import SumCost
-from .sum_cost_factory import SumCostFactory
 from .cost import Cost
-from .cost_factory import CostFactory
