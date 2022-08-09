82434858224596a9438585f5b57d25b56484a6c2
Dohun <dohunj2@illinois.edu>
2022-08-08 23:06:15 -0500

    Parallel Control Evaluator

--- autompc/tuning/control_tuner.py
+++ autompc/tuning/control_tuner.py
@@ -20,7 +20,7 @@ from ..trajectory import Trajectory
 from ..dynamics import Dynamics
 from .model_tuner import ModelTuner, ModelTunerResult
 from .model_evaluator import ModelEvaluator
-from .control_evaluator import ControlEvaluator, StandardEvaluator, ControlEvaluationTrial, trial_to_json
+from .control_evaluator import ControlEvaluator, ParallelStandardEvaluator, StandardEvaluator, ControlEvaluationTrial, trial_to_json
 from .control_performance_metric import ControlPerformanceMetric,ConfidenceBoundPerformanceMetric
 from .bootstrap_evaluator import BootstrapSurrogateEvaluator
 from .smac_runner import SMACRunner
@@ -253,7 +253,7 @@ class ControlTuner:
             print("Skipping surrogate tuning, surrogate is a trained model")
             print("------------------------------------------------------------------")
             if control_evaluator is None:
-                control_evaluator = StandardEvaluator(controller.system, task, surrogate, 'surr_')
+                control_evaluator = ParallelStandardEvaluator(controller.system, task, surrogate, 'surr_')
             else:
                 assert not isinstance(control_evaluator,BootstrapSurrogateEvaluator),'Need an evaluator that does not train'
         else:
