3cdef5802bc6624ff98cbc06909159eb7c52e220
Dohun <dohunj2@illinois.edu>
2022-08-31 16:05:42 -0500

    Debug logging
    Planned trajectories, timeout and barrier violation logging

--- autompc/costs/__init__.py
+++ autompc/costs/__init__.py
@@ -1,4 +1,4 @@
 from .quad_cost import QuadCost
 from .thresh_cost import ThresholdCost, BoxThresholdCost
 from .barrier_cost import LogBarrierCost
-from .cost import Cost
+from .cost import Cost, SumCost
